4f96989ab43f7d94e7d5438ae0adf7ce88b6b5fa
Todd Kaiser <marvin826@comcast.net>
2012-12-27 16:40:45 -0700

    Cleaned up XbeePacket class to deal with partial packets

--- src/XbeePacket.py
+++ src/XbeePacket.py
@@ -2,146 +2,177 @@
 
 class XbeePacket:
 
-	def __init__(self,escaped=False):
+    def __init__(self, escaped=False):
 
-		self.rawBytes = []
-		self.processedBytes = []
-		self.withEscapeChars = escaped
-		self.frameDataLength = -1
-		self.packetChecksum = None
-		self.calculatedChecksum = None
-		self.isWholePacket = False
-		self.isValidPacket = True
-		self.totalPacketLength = -1
+        self.rawBytes = []
+        self.processedBytes = []
+        self.withEscapeChars = escaped
+        self.frameDataLength = -1
+        self.packetChecksum = None
+        self.calculatedChecksum = None
+        self.isWholePacket = False
+        self.isErrorFreePacket = True
+        self.totalPacketLength = -1
 
-	def __str__(self):
-		tempStr = "raw bytes length: " + str(len(self.rawBytes)) + \
-		          "\nwithEscapeChars: " + str(self.withEscapeChars) + \
-		          "\nframe data lenth: " + str(self.frameDataLength) + \
-		          "\npacket checksum: " + str(hex(self.packetChecksum)) + \
-		          "\ncalculated checksum: " + str(hex(self.calculatedChecksum)) + \
-		          "\nisWholePacket: " + str(self.isWholePacket) + \
-		          "\nisValidPacket: " + str(self.isValidPacket) + \
-		          "\ntotalPacketLength: " + str(self.totalPacketLength)
-		tempStr += "\nRaw Bytes:\n"
-		tempStr += self.bytesToStr(self.rawBytes)
-		tempStr += "Processed Bytes:\n"
-		tempStr += self.bytesToStr(self.processedBytes)
-		
-		return tempStr
+    def __str__(self):
+        tempStr = "raw bytes length: " + str(len(self.rawBytes)) + \
+                  "\nwithEscapeChars: " + str(self.withEscapeChars) + \
+                  "\nframe data lenth: " + str(self.frameDataLength) + \
+                  "\npacket checksum: " + str(hex(self.packetChecksum)) + \
+                  "\ncalculated checksum: " + \
+                  str(hex(self.calculatedChecksum)) + \
+                  "\nisWholePacket: " + str(self.isWholePacket) + \
+                  "\nisErrorFreePacket: " + str(self.isErrorFreePacket) + \
+                  "\ntotalPacketLength: " + str(self.totalPacketLength)
+        tempStr += "\nRaw Bytes:\n"
+        tempStr += self.bytesToStr(self.rawBytes)
+        tempStr += "Processed Bytes:\n"
+        tempStr += self.bytesToStr(self.processedBytes)
 
-	def getFrameDataLenth(self):
-		return self.frameDataLength
+        return tempStr
 
-	def getRawBytes(self):
-		return self.getRawBytes
+    def getFrameDataLenth(self):
+        return self.frameDataLength
 
-	def getChecksum(self):
-		return self.checksum
+    def getRawBytes(self):
+        return self.getRawBytes
 
-	def setDelimitMode(self,mode):
-		self.delimitMode = mode
+    def getProcessedBytes(self):
+        return self.getProcessedBytes
 
-	def setFrameDataLength(self,length):
-		self.frameDataLength = length
-		self.calculatePacketLength()
+    def getPacketChecksum(self):
+        return self.packetChecksum
 
-	def pushRawBytes(self,raw_bytes):
+    def getIsWholePacket(self):
+        return self.isWholePacket
 
-		# don't take these if we're a whole
-		# packet already
-		if(self.isWholePacket):
-			raise Exception("XbeePacket: Error: pushing raw bytes to whole packet")
+    def getIsValidPacket(self):
+        return self.isWholePacket and self.isErrorFreePacket
 
-		if(len(self.rawBytes) == 0):
-			# make sure the first byte is '\x7e'
-			if(raw_bytes[0] != '\x7e'):
-				raise Exception("Bad bytes received for new packet")
-		for raw_byte in raw_bytes:
-			self.rawBytes.append(ord(raw_byte))
-		
+    def setDelimitMode(self, mode):
+        self.delimitMode = mode
 
-		# see if we have at least the packet identifier and 
-		# frame packet data length bytes so far
-		if(len(self.rawBytes)>=3):
-			# get the length of the frame packet data
-			msb = self.rawBytes[1]
-			lsb = self.rawBytes[2]
-			self.setFrameDataLength((msb << 8) | lsb)
+    def setFrameDataLength(self, length):
+        self.frameDataLength = length
+        self.calculatePacketLength()
 
-		# if the calculated packet length is 
-		# equal to the number of raw bytes we have, 
-		# then we have a complete packet
-		if(self.totalPacketLength == len(self.rawBytes)):
-			self.isWholePacket = True
+    def pushRawBytes(self, raw_bytes):
 
-			# deal with any escaped bytes
-			self.processedBytes = self.processEscapedBytes(self.rawBytes)
+        # buffer for bytes that are not mine. I may
+        # receive a buffer that has my bytes and bytes
+        # of the next packet in it. we take our bytes
+        # and return the rest to the caller
+        extra_bytes = []
+        my_bytes = raw_bytes
 
-			# grab the checksum
-			self.packetChecksum = self.rawBytes[-1:][0]
+        # don't take these if we're a whole
+        # packet already
+        if(self.isWholePacket):
+            raise Exception(
+                "XbeePacket: Error: pushing raw bytes to whole packet")
 
-			# calculate the checksum
-			self.calculateChecksum()
+        if(len(self.rawBytes) == 0):
+            # make sure the first byte is '\x7e'
+            if(raw_bytes[0] != '\x7e'):
+                raise Exception("Bad bytes received for new packet")
 
-			print self
+        # look to see if we have a partial -- meaning that
+        # a new packet is starting in the middle of the raw_bytes
+        # given. If so, take just the bytes up to the delimiter
+        # and return the rest
+        for i in range(len(raw_bytes)):
+            rbyte = raw_bytes[i]
+            delim_index = -1
+            if(rbyte == '\x7e' and i > 0):
+                delim_index = i
+                my_bytes = raw_bytes[:i]
+                extra_bytes = raw_bytes[i:]
 
-		return self.isWholePacket
+        for raw_byte in my_bytes:
+            self.rawBytes.append(ord(raw_byte))
 
-	def calculatePacketLength(self):
-		# recalulate the total packet length
-		#
-		# length is determined by the frame packet data length,
-		# plus the first 3 bytes (header, length msb, length lsb) 
-		# and the checksum byte on the end
-		# also, if the packet is in delimited mode, we have
-		# to account for delimiter bytes
-		self.totalPacketLength = self.frameDataLength + 4
-		delimCount = 0
-		for raw_byte in self.rawBytes:
-			if(raw_byte == 0x7d):
-				delimCount += 1
-		self.totalPacketLength += delimCount
+        # see if we have at least the packet identifier and
+        # frame packet data length bytes so far
+        if(len(self.rawBytes) >= 3):
+            # get the length of the frame packet data
+            msb = self.rawBytes[1]
+            lsb = self.rawBytes[2]
+            self.setFrameDataLength((msb << 8) | lsb)
 
-	def calculateChecksum(self):
-		# we calculate the checksum by
-		# adding the bytes after the first
-		# three bytes (delimiter and length)
-		# and subtracting this total from 0xFF
-		if(self.isWholePacket):
-			total = 0
-			for raw_byte in self.processedBytes[3:-1]:
-				total += raw_byte
-			self.calculatedChecksum = 0xff - (total & 0xff)
+        # if the calculated packet length is
+        # equal to the number of raw bytes we have,
+        # then we have a complete packet
+        if(self.totalPacketLength == len(self.rawBytes)):
+            self.isWholePacket = True
 
-		# verify that this is not a corrupt packet
-		if(self.calculatedChecksum!=self.packetChecksum):
-			raise Exception("XbeePacket : Error: Received packet is corrupted")
-		else:
-			self.isValidPacket = True
+            # deal with any escaped bytes
+            self.processedBytes = self.processEscapedBytes(self.rawBytes)
 
-	def processEscapedBytes(self, raw_bytes):
-		processed = []
-		escaped = False
-		for b in raw_bytes:
-			if(b == 0x7d and self.withEscapeChars):
-				escaped = True
-				continue
-			if(escaped):
-				escaped = False
-				b = b ^ 0x20
-			processed.append(b)
-		return processed
+            # grab the checksum. make sure the checksum was not
+            # delimted
+            checksum = self.rawBytes[-1:][0]
+            if(self.rawBytes[-2:-1][0] == 0x7d):
+                checksum = checksum ^ 0x20
+            self.packetChecksum = checksum
 
-	def bytesToStr(self, rawBytes):
-		length = len(rawBytes)
-		tempStr = str(length) + " ["
-		for i in range(length):
-			tempStr += "\'" + hex(rawBytes[i]) + "\'"
-			if(i < (length - 1)):
-				tempStr += ', '
-		tempStr += "]\n"
-		return tempStr
+            # calculate the checksum
+            self.calculateChecksum()
 
-	def isWholePacket(self):
-		return self.isWholePacket
+        return extra_bytes
+
+    def calculatePacketLength(self):
+        # recalulate the total packet length
+        #
+        # length is determined by the frame packet data length,
+        # plus the first 3 bytes (header, length msb, length lsb)
+        # and the checksum byte on the end
+        # also, if the packet is in delimited mode, we have
+        # to account for delimiter bytes
+        self.totalPacketLength = self.frameDataLength + 4
+        delimCount = 0
+        for raw_byte in self.rawBytes:
+            if(raw_byte == 0x7d):
+                delimCount += 1
+        self.totalPacketLength += delimCount
+
+    def calculateChecksum(self):
+        # we calculate the checksum by
+        # adding the bytes after the first
+        # three bytes (delimiter and length)
+        # and subtracting this total from 0xFF
+        if(self.isWholePacket):
+            total = 0
+            for raw_byte in self.processedBytes[3:-1]:
+                total += raw_byte
+            self.calculatedChecksum = 0xff - (total & 0xff)
+
+            # verify that this is not a corrupt packet
+            if(self.calculatedChecksum != self.packetChecksum):
+                raise Exception(
+                    "XbeePacket : Error: Received packet is corrupted" +
+                    str(self))
+            else:
+                self.isErrorFreePacket = True
+
+    def processEscapedBytes(self, raw_bytes):
+        processed = []
+        escaped = False
+        for b in raw_bytes:
+            if(b == 0x7d and self.withEscapeChars):
+                escaped = True
+                continue
+            if(escaped):
+                escaped = False
+                b = b ^ 0x20
+            processed.append(b)
+        return processed
+
+    def bytesToStr(self, rawBytes):
+        length = len(rawBytes)
+        tempStr = str(length) + " ["
+        for i in range(length):
+            tempStr += "\'" + hex(rawBytes[i]) + "\'"
+            if(i < (length - 1)):
+                tempStr += ', '
+        tempStr += "]\n"
+        return tempStr
--- src/exit_handler.py
+++ src/exit_handler.py
@@ -1,13 +1,15 @@
-import os, sys
+import os
+import sys
+
 
 def set_exit_handler(func):
-	if os.name == 'nt':
-		try:
-			import win32api
-			win32api.SetConsoleCtrlHandler(func,True)
-		except ImportError:
-			version = '.'.join(map(str, sys.version_info[:2]))
-			raise Exception("pywin32 not installed for Python " + version)
-	else:
-		import signal
-		signal.signal(signal.SIGTERM, func)
+    if os.name == 'nt':
+        try:
+            import win32api
+            win32api.SetConsoleCtrlHandler(func, True)
+        except ImportError:
+            version = '.'.join(map(str, sys.version_info[:2]))
+            raise Exception("pywin32 not installed for Python " + version)
+    else:
+        import signal
+        signal.signal(signal.SIGTERM, func)
