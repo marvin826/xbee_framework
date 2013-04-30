dac5d72d8c5c93bdba554644d4a88a7402c23cc0
Todd Kaiser <marvin826@comcast.net>
2013-04-29 21:17:38 -0600

    Moved common test config data into its own module.

---
+++ xbeeframework/test/test_configuration.py
@@ -0,0 +1,11 @@
+import sys
+
+project_root = "j:\\Users\\Todd\\Projects\\ZigBee\\XBeeFramework"
+sys.path.append(project_root + '\\xbeeframework')
+
+configuration = {
+    "dbFilename": project_root + "\\data\\XBee_API_Frame_Database.db",
+    "commPort": "COM6",
+    "logFile": project_root + "\\XBeeFramework.log"
+
+}
